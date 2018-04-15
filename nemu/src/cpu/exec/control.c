57ea4162c52e0d27d26499ad129469b1b0d09aab
tracer-ics2017 <tracer@njuics.org>
2018-04-15 11:03:42 +0800

    >  compile 161630218 lianghao Linux iZwz9h2vcoigafyzqkpltvZ 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  11:03:42 up 6 days, 16:15, 10 users,  load average: 0.00, 0.00, 0.00 5264c4273b853b52a33cc3aa5deb25da9a098dd9

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -26,7 +26,7 @@ make_EHelper(jmp_rm) {
 make_EHelper(call) {
   // the target address is calculated at the decode stage
   rtl_push(&cpu.eip+4);
-  printf("decoding.jmp_eip: %d\n id_dest->val:%d\nid_src->val:%d\n", decoding.jmp_eip,id_dest->val,id_src->val);
+  printf("decoding.jmp_eip: %d\n id_dest->val:%d\nid_src->val:%d\nid_src2->val: %d\n", decoding.jmp_eip,id_dest->val,id_src->val,id_src2->val);
   rtl_mv(&cpu.eip, &decoding.jmp_eip); 
   print_asm("call %x", decoding.jmp_eip);
 }
