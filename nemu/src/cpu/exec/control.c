9fef234ce85a87714a0d61f85d8dcad816772c34
tracer-ics2017 <tracer@njuics.org>
2018-04-16 19:44:47 +0800

    >  compile 161630218 lianghao Linux iZwz9h2vcoigafyzqkpltvZ 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:44:47 up 8 days, 57 min,  4 users,  load average: 0.00, 0.00, 0.00 41b341e9be143380345941f11ad1d8c5bb0f03d0

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -29,7 +29,7 @@ make_EHelper(call) {
   rtl_push(&cpu.eip+4);
   printf("decoding.jmp_eip: %d\n id_dest->val:%d\nid_src->val:%d\nid_src2->val: %d\n", decoding.jmp_eip,id_dest->val,id_src->val,id_src2->val);
   rtl_addi(&cpu.eip, &cpu.eip, id_src->val);
-  decoding.jmp_eip = id_dest->val;
+  decoding.jmp_eip = cpu.eip;
   print_asm("call %x", decoding.jmp_eip);
 }
 
