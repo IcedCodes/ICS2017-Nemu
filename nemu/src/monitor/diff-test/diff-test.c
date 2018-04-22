49bbbf6363c59747874b3426e6c55ac5598ca1eb
tracer-ics2017 <tracer@njuics.org>
2018-04-22 19:08:25 +0800

    >  compile 161630218 lianghao Linux iZwz9h2vcoigafyzqkpltvZ 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:08:25 up 14 days, 20 min,  7 users,  load average: 0.00, 0.00, 0.00 94f5c170a193ba58bee8367e654d6ab691c31d11

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -149,7 +149,7 @@ void difftest_step(uint32_t eip) {
 
   // TODO: Check the registers state with QEMU.
   // Set `diff` as `true` if they are not the same.
-  if(r.eax != cpu.eax || r.ebx != cpu.ecx || r.ecx != cpu.ecx || r.edx != cpu.edx || r.esp != cpu.esp || r.ebp != cpu.ebp || r.esi != cpu.esi ||r.edi != cpu.edi || r.eip != cpu.eip)
+  if(r.eax != cpu.eax || r.ebx != cpu.ebx || r.ecx != cpu.ecx || r.edx != cpu.edx || r.esp != cpu.esp || r.ebp != cpu.ebp || r.esi != cpu.esi ||r.edi != cpu.edi || r.eip != cpu.eip)
   {
 		  diff = true;
 		  printf("There is some differences between Nemu and Qemu!\n");
