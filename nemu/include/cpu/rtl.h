ac4718ea6b0ee730689a47683a09a30dde4445ff
tracer-ics2017 <tracer@njuics.org>
2018-04-26 19:33:58 +0800

    >  compile 161630218 lianghao Linux iZwz9h2vcoigafyzqkpltvZ 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:33:58 up 18 days, 46 min,  9 users,  load average: 0.08, 0.02, 0.01 1c20730a78af1732bc2ea3a86f5b8292531e7d20

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -175,7 +175,9 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  TODO();
+  uint32_t a = ((uint32_t)-1) >> (32 - width * 8);
+  rtlreg_t b = (a & *result) == 0;
+  rtl_set_ZF(&b);
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
