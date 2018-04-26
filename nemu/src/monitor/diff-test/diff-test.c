a9760fcd10137246ac9119eaecbb7670c0dfa86f
tracer-ics2017 <tracer@njuics.org>
2018-04-26 19:42:10 +0800

    >  compile 161630218 lianghao Linux iZwz9h2vcoigafyzqkpltvZ 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:42:10 up 18 days, 54 min, 10 users,  load average: 0.01, 0.01, 0.00 70aa1d6a885e679a2c557475b8c117483f6e7ae4

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -149,10 +149,11 @@ void difftest_step(uint32_t eip) {
 
   // TODO: Check the registers state with QEMU.
   // Set `diff` as `true` if they are not the same.
+  diff = true;
   if(r.eax != cpu.eax || r.ebx != cpu.ebx || r.ecx != cpu.ecx || r.edx != cpu.edx || r.esp != cpu.esp || r.ebp != cpu.ebp || r.esi != cpu.esi ||r.edi != cpu.edi || r.eip != cpu.eip)
   {
 		  diff = true;
-		  printf("There is some differences between Nemu and Qemu!\n");
+		  printf("There are some differences between Nemu and Qemu!\n");
 		  printf("Regs\t   Qemu\t\t\t   Nemu\n");
 		  printf("eax\t 0x%08x\t\t 0x%08x\n", r.eax, cpu.eax);
 		  printf("ebx\t 0x%08x\t\t 0x%08x\n", r.ebx, cpu.ebx);
