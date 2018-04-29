99cb3fe53087032becbc11f81c74fdfadc55d6a8
tracer-ics2017 <tracer@njuics.org>
2018-04-29 22:15:28 +0800

    >  compile 161630218 lianghao Linux iZwz9h2vcoigafyzqkpltvZ 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:15:28 up 21 days,  3:27,  9 users,  load average: 0.00, 0.00, 0.00 c3884aad1bd91e57dc39a8f611874785e0038d3a

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -13,10 +13,10 @@ make_EHelper(test) {
 make_EHelper(and) {
   //扩展填充
   printf("id_dest=%x id_src=%x id_src.width=%x dest_width=%x\n",id_dest->val,id_src->val,id_src->width,id_dest->width);
-  rtl_sext(&t2,&id_src->val,id_src->width);
-  rtl_and(&t1, &id_dest->val ,&t2);
-  operand_write(id_dest, &t1);
-  printf("t1: %x\n", t1);
+  rtl_sext(&t0,&id_src->val,id_src->width);
+  rtl_and(&t2, &id_dest->val ,&t0);
+  operand_write(id_dest, &t2);
+  printf("t0: %x\n", t0);
   t0 = 0;
   rtl_set_CF(&t0);
   rtl_set_OF(&t0);
