b538abb9157086ff6c445e18bb93a2594dec3221
tracer-ics2017 <tracer@njuics.org>
2018-04-30 15:35:07 +0800

    >  compile 161630218 lianghao Linux iZwz9h2vcoigafyzqkpltvZ 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  15:35:07 up 21 days, 20:47,  9 users,  load average: 0.01, 0.04, 0.01 a8f07422bd0f31cafe95f9e8ba37036a6196d741

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -49,11 +49,10 @@ make_EHelper(inc) {
   t1 = 1;
   rtl_add(&t2, &id_dest->val, &t1);
   operand_write(id_dest, &t2);
-  rtl_update_ZFSF(&t1, id_dest->width);
-  rtl_sltu(&t0, &id_dest->val, &t1);
-  rtl_set_CF(&t0);
+  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_sltu(&t0, &t2, &id_dest->val);
   rtl_xor(&t0, &id_dest->val, &id_src->val);
-  rtl_xor(&t2, &id_dest->val, &t1);
+  rtl_xor(&t1, &id_dest->val, &t2);
   rtl_and(&t0, &t0, &t1);
   rtl_msb(&t0, &t0, id_dest->width);
   rtl_set_OF(&t0);
