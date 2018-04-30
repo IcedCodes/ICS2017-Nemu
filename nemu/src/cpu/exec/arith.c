0b8415d4820bd0a3bbea07546b00ec8aa7b327a5
tracer-ics2017 <tracer@njuics.org>
2018-04-30 14:41:40 +0800

    >  compile 161630218 lianghao Linux iZwz9h2vcoigafyzqkpltvZ 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:41:40 up 21 days, 19:53,  9 users,  load average: 0.00, 0.00, 0.00 b9326cfc62023aa184fc9107bdf68e922851255a

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -25,7 +25,7 @@ make_EHelper(sub) {
   rtl_set_CF(&t0);
   rtl_xor(&t0, &id_dest->val, &id_src->val);
   rtl_xor(&t2, &id_dest->val, &t1);
-  rtl_and(&t0, &t0, &t1);
+  rtl_and(&t0, &t0, &t2);
   rtl_msb(&t0, &t0, id_dest->width);
   rtl_set_OF(&t0);
   print_asm_template2(sub);
