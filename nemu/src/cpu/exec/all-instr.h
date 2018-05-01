512fb0ed11e11b0614ca493d99041ac23a88c56d
tracer-ics2017 <tracer@njuics.org>
2018-05-01 12:31:43 +0800

    >  compile 161630218 lianghao Linux lianghao-pc 4.4.0-2-deepin-686-pae #1 SMP Deepin 4.4.6-4 (2016-07-01) i686 GNU/Linux  12:31:43 up 46 min,  1 user,  load average: 0.34, 0.52, 0.57 9605fbcf6684cba4cc13e3f9fc25d5af3c3706

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -42,3 +42,5 @@ make_EHelper(jmp_rm);
 make_EHelper(div);
 make_EHelper(in);
 make_EHelper(out);
+make_EHelper(neg);
+make_EHelper(mul);
--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -78,7 +78,9 @@ make_EHelper(dec) {
 }
 
 make_EHelper(neg) {
-  TODO();
+  rtl_set_CF(&id_dest->val);
+  id_dest->val = -id_dest->val;
+  operand_write(id_dest, &id_dest->val);
 
   print_asm_template1(neg);
 }
