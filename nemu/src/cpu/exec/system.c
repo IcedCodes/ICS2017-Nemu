0d16837fb92647ab63e7eb6294bbe6291395dced
tracer-ics2017 <tracer@njuics.org>
2018-05-05 15:10:41 +0800

    >  compile 161630218 lianghao Linux lianghao-pc 4.4.0-2-deepin-686-pae #1 SMP Deepin 4.4.6-4 (2016-07-01) i686 GNU/Linux  15:10:41 up  9:11,  1 user,  load average: 0.39, 0.21, 0.19 4360ab3f6ab6bf6632fa4433cad7e38671dfcdd4

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -45,7 +45,8 @@ uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  reg_l(R_EAX) = pio_read(reg_w(R_EDX), id_dest->width);
+  t1 = pio_read(id_src->val, id_src->width);
+  operand_write(id_dest, &t1);
 
   print_asm_template2(in);
 
@@ -55,7 +56,7 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  pio_write(reg_w(R_EDX), id_dest->width, reg_l(R_EAX));
+  pio_write(id_dest->val, id_src->val, id_src->width);
 
   print_asm_template2(out);
 
