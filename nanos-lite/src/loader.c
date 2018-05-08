a3154a2e4cf5fe39a787fd6bf81d4fa2b455174b
tracer-ics2017 <tracer@njuics.org>
2018-05-08 19:42:50 +0800

    >  compile 161630218 lianghao Linux lianghao-pc 4.4.0-2-deepin-686-pae #1 SMP Deepin 4.4.6-4 (2016-07-01) i686 GNU/Linux  19:42:50 up  2:23,  2 users,  load average: 0.28, 0.12, 0.07 62b0e0b6dd315d6091bf771bdb2e5d615b55c447

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,9 @@
 #include "common.h"
+#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
