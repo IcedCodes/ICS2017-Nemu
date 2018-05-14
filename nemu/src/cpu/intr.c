9c164f65149ec2d2384f090a129acb5c14ac8402
tracer-ics2017 <tracer@njuics.org>
2018-05-14 17:36:27 +0800

    >  compile 161630218 lianghao Linux lianghao-pc 4.4.0-2-deepin-686-pae #1 SMP Deepin 4.4.6-4 (2016-07-01) i686 GNU/Linux  17:36:27 up  9:59,  1 user,  load average: 0.14, 0.22, 0.15 223d172c3d246267a668c0717e69545d264d7644

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,13 +6,14 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
    rtl_push(&cpu.eflags.value);
-   rtl_push((uint32_t*)&cpu.cs);
+   uint32_t a = cpu.cs;
+   rtl_push(&a);
    rtl_push(&ret_addr);
    uint32_t low, high;
    low = vaddr_read(cpu.idtr.base + NO*8, 4) & 0x0000ffff;
    high = vaddr_read(cpu.idtr.base + NO*8 + 4, 4) & 0xffff0000;
    decoding.is_jmp = 1;
-   decoding.jmp_eip = low + high;
+   decoding.jmp_eip = low | high;
 }
 
 void dev_raise_intr() {
