97a9c6d5385d2d0e2389e81e89584ab60cd79847
tracer-ics2017 <tracer@njuics.org>
2018-05-21 22:28:15 +0800

    >  compile 161630218 lianghao Linux lianghao-pc 4.4.0-2-deepin-686-pae #1 SMP Deepin 4.4.6-4 (2016-07-01) i686 GNU/Linux  22:28:15 up 58 min,  1 user,  load average: 0.50, 0.41, 0.25 d69afe13229d8928c4573058d55cf3dc1acd838f

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -7,11 +7,13 @@
 #define PGSIZE    4096    // Bytes mapped by a page
 
 struct _RegSet {
-  uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
   int       irq;
+  uintptr_t error_code;
+  uintptr_t cs, eip, eflags;
 };
 
-#define SYSCALL_ARG1(r) 0
+#define SYSCALL_ARG1(r) r->eax
 #define SYSCALL_ARG2(r) 0
 #define SYSCALL_ARG3(r) 0
 #define SYSCALL_ARG4(r) 0
